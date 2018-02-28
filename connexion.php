25a9032b47cb1ff1525d193efd894f7b635b768a
Alexis Cadorette <alecado.cegepthetford.ca>
2018-02-28 10:30:29 -0500

    Debug test#2

--- connexion.php
+++ connexion.php
@@ -4,9 +4,10 @@
   $id = intval($_GET['id']);
   $_SESSION['id'] = $id;
   $sql = "SELECT count(*) AS nb FROM utilisateur WHERE loginID={$id};";
-  $resultats = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-  if($_SESSION['newUser'] = ($resultats[0]['nb'] == 0)) {
-    header("Location: ./nouvelUtilisateur.php?test=$resultats[0]['nb']");
+  $resultat = $db->query($sql)->fetch();
+  if($_SESSION['newUser'] = ($resultat['nb'] == 0)) {
+    header("Location: ./nouvelUtilisateur.php");
+    echo $_SESSION['newUser'];
   }
   else {
     header("Location: ./feed.php?id={$_GET['id']}");
