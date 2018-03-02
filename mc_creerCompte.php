50de07b8e156465383b16f0d5515ee6403ae59c9
TheMasterCado <master_cado@hotmail.com>
2018-03-02 16:02:35 -0500

    mc_creerCompte -> prend les valeurs du form

--- mc_creerCompte.php
+++ mc_creerCompte.php
@@ -1,9 +1,10 @@
 <?php
   require 'bd.php';
-  $sql = "INSERT INTO utilisateur (utilisateur.nom, utilisateur.prenom, utilisateur.nb_session, utilisateur.loginId, utilisateur.image, utilisateur.email) VALUES ('".$_POST['nom']."', '".$_POST['prenom']."', '".$_POST['nbSessions']."', 1, 'https://us-east-1.tchyn.io/snopes-production/uploads/2017/10/trump_chin_faux_feature.jpg?resize=865%2C452', 'jondubo@dinfo.cegepthetford.ca');";
+  $sql = "INSERT INTO utilisateur (utilisateur.nom, utilisateur.prenom, utilisateur.nb_session, utilisateur.loginId, utilisateur.image, utilisateur.email)
+          VALUES ('".$_POST['nom']."', '".$_POST['prenom']."', '".$_POST['nbSessions']."', '".$_POST['id']."', '".$_POST['image']."', '".$_POST['email']."');";
   $stmt = $db->prepare($sql);
   $stmt->execute();
   session_start();
   unset($_SESSION['newUser']);
-  header('Location: ./index.php');
+  header('Location: ./feed');
  ?>
--- nouvelUtilisateur.php
+++ nouvelUtilisateur.php
@@ -19,19 +19,19 @@ header("Location: ./index.php");
 <h1>Créer un compte</h1>
 <form id="formulaire" class="" action="./mc_creerCompte.php" method="post">
   <label for="prenom">Prénom</label>
-  <input type="text" name="prenom" value="<?=$_SESSION['prenom']?>">
+  <input type="text" name="prenom" value="<?=$_SESSION['newUser']['prenom']?>" required>
   <br>
   <label for="nom">Nom</label>
-  <input type="text" name="nom" value="<?=$_SESSION['nom']?>">
+  <input type="text" name="nom" value="<?=$_SESSION['newUser']['nom']?>" required>
   <br>
   <label for="nbSessions">Nombre de sessions en informatique</label>
-  <input type="number" name="nbSessions" min="1" max="6">
+  <input type="number" name="nbSessions" min="1" max="6" required>
   <br>
   <label for="specialite">Spécialité (facultatif)</label>
-  <input type="text" name="specialite" value="">
+  <input type="text" name="specialite">
   <br>
-  <input type="hidden" name="email" value="<?=$_SESSION['email']?>">
-  <input type="hidden" name="image" value="<?=$_SESSION['image']?>">
+  <input type="hidden" name="email" value="<?=$_SESSION['newUser']['email']?>">
+  <input type="hidden" name="image" value="<?=$_SESSION['newUser']['image']?>">
   <input type="hidden" name="id" value="<?=$_SESSION['id']?>">
   <input type="submit" value="Valider">
 </form>
