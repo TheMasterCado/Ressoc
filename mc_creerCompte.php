ce551879962b807ce8029d4517297cd49b910f50
Dubois Jonathan <1635764@cegepthetford.ca>
2018-03-05 16:32:04 -0500

    creerCompte sql

--- mc_creerCompte.php
+++ mc_creerCompte.php
@@ -1,7 +1,16 @@
 <?php
   require 'bd.php';
-  $sql = "INSERT INTO utilisateur (utilisateur.nom, utilisateur.prenom, utilisateur.nb_session, utilisateur.loginId, utilisateur.image, utilisateur.email)
-          VALUES ('".$_POST['nom']."', '".$_POST['prenom']."', '".$_POST['nbSessions']."', '".$_POST['id']."', '".$_POST['image']."', '".$_POST['email']."');";
+  $sql = "SELECT COUNT(*) AS nb FROM specialite WHERE nom = '"$_POST['specialite']"'";
+  $resultat = $db->query($sql)->fetch();
+  if($resultat['nb'] == 0){
+  $sql = "INSERT INTO specialite (specialite.nom) VALUES ($_POST['specialite'])";
+  $stmt = $db->prepare($sql);
+  $stmt->execute();
+  }
+  $sql = "SELECT pk_specialite FROM specialite WHERE nom='"$_POST['specialite']"'";
+  $resultat = $db->query($sql)->fetch();
+  $sql = "INSERT INTO utilisateur (utilisateur.nom, utilisateur.prenom, utilisateur.nb_session, utilisateur.loginId, utilisateur.image, utilisateur.email, utilisateur.specialite)
+          VALUES ('".$_POST['nom']."', '".$_POST['prenom']."', '".$_POST['nbSessions']."', '".$_POST['id']."', '".$_POST['image']."', '".$_POST['email']."', '"$resultat['pk_specialite']"');";
   $stmt = $db->prepare($sql);
   $stmt->execute();
   session_start();
