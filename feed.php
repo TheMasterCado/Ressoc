0fcbf8e872dd3fc81e9152f0fbaffa320dded0ec
Dubois Jonathan <1635764@cegepthetford.ca>
2018-03-05 15:55:41 -0500

    début feed.php

--- feed.php
+++ feed.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+if(!isset($_SESSION['id']))
+  header("Location: ./index.php");
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -5,6 +10,13 @@
     <title>feed</title>
   </head>
   <body>
-
+<?php
+  require 'bd.php';
+  $sql = "SELECT * FROM publication WHERE loginID={$_SESSION['id']}";
+  $resultat = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($resultat as $pos => $publication) {
+    $publication['texte']
+  }
+?>
   </body>
 </html>
