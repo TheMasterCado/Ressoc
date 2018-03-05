3f02b2847773bb387be69554eed9e73b1af7d2e2
Dubois Jonathan <1635764@cegepthetford.ca>
2018-03-05 14:08:42 -0500

    oops

--- nouvelUtilisateur.php
+++ nouvelUtilisateur.php
@@ -19,10 +19,10 @@ header("Location: ./index.php");
 <h1>Créer un compte</h1>
 <form id="formulaire" class="" action="./mc_creerCompte.php" method="post">
   <label for="prenom">Prénom</label>
-  <input type="text" name="prenom" value="<?=$_POST['newUser']['prenom']?>" required>
+  <input type="text" name="prenom" value="<?=$_SESSION['newUser']['prenom']?>" required>
   <br>
   <label for="nom">Nom</label>
-  <input type="text" name="nom" value="<?=$_POST['newUser']['nom']?>" required>
+  <input type="text" name="nom" value="<?=$_SESSION['newUser']['nom']?>" required>
   <br>
   <label for="nbSessions">Nombre de sessions en informatique</label>
   <input type="number" name="nbSessions" min="1" max="6" required>
@@ -30,9 +30,9 @@ header("Location: ./index.php");
   <label for="specialite">Spécialité (facultatif)</label>
   <input type="text" name="specialite">
   <br>
-  <input type="hidden" name="email" value="<?=$_POST['newUser']['email']?>">
-  <input type="hidden" name="image" value="<?=$_POST['newUser']['image']?>">
-  <input type="hidden" name="id" value="<?=$_POST['id']?>">
+  <input type="hidden" name="email" value="<?=$_SESSION['newUser']['email']?>">
+  <input type="hidden" name="image" value="<?=$_SESSION['newUser']['image']?>">
+  <input type="hidden" name="id" value="<?=$_SESSION['id']?>">
   <input type="submit" value="Valider">
 </form>
 </body>
