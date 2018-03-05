5fffdbcf6c5eaee00e0fd809d37c5f905d1631b3
TheMasterCado <master_cado@hotmail.com>
2018-03-05 17:38:22 -0500

    Think its ok

--- mc_creerCompte.php
+++ mc_creerCompte.php
@@ -9,17 +9,12 @@ if($resultat['nb'] == 0){
 }
 $sql = "SELECT pk_specialite FROM specialite WHERE nom = '".$_POST['specialite']."'";
 $specialite = $db->query($sql)->fetch();
-var_dump($_POST);
 $sql = "INSERT INTO utilisateur (utilisateur.nom, utilisateur.prenom, utilisateur.nb_session, utilisateur.loginId, utilisateur.image, utilisateur.email, utilisateur.specialite)
         VALUES ('".$_POST['nom']."', '".$_POST['prenom']."', '".intval($_POST['nbSessions'])."', '".intval($_POST['id'])."', '".$_POST['image']."', '".$_POST['email']."', '".$specialite['pk_specialite']."');";
  $stmt = $db->prepare($sql);
  $stmt->execute();
  echo "FUCK";
- // try{
- //   session_start();
- //   echo "session";
- //   unset($_SESSION['newUser']);
- //   echo "unset";
- //   header("Location: ./feed.php?id={$_SESSION['id']}");
- // }catch{echo "oups";}
+ session_start();
+ unset($_SESSION['newUser']);
+ header("Location: ./feed.php?id={$_SESSION['id']}");
 ?>
