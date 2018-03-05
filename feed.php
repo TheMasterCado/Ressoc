e44bee29cd182c94bfbe6b7076182adfeaaf98b1
TheMasterCado <master_cado@hotmail.com>
2018-03-05 18:55:01 -0500

    everything is ok

--- feed.php
+++ feed.php
@@ -2,7 +2,7 @@
 session_start();
 if(isset($_SESSION['id'])) {
   if(isset($_SESSION['newUser']))
-    header("Location: ./nouvelUtilisateur.php");
+  header("Location: ./nouvelUtilisateur.php");
 }
 else {
   header("Location: ./index.php");
@@ -10,21 +10,19 @@ else {
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>feed</title>
-  </head>
-  <body>
-<?php
-echo $_GET['id'];
-echo intval($_GET['id']);
-  // require 'bd.php';
-  // $sql = "SELECT * FROM publication WHERE fk_utilisateur =
-  //         (SELECT pk_utilisateur FROM utilisateur WHERE loginID = {$_GET['id']});";
-  // $resultat = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-  // foreach ($resultat as $pos => $publication) {
-  //   echo $publication['texte'];
-  // }
-?>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <title>feed</title>
+</head>
+<body>
+  <?php
+  require 'bd.php';
+  $sql = "SELECT * FROM publication WHERE fk_utilisateur =
+  (SELECT pk_utilisateur FROM utilisateur WHERE loginID = {$_GET['id']});";
+  $resultat = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($resultat as $pos => $publication) {
+    echo $publication['texte'];
+  }
+  ?>
+</body>
 </html>
