89ebda52b880659460e0410e4ab38fe25f101826
TheMasterCado <master_cado@hotmail.com>
2018-03-05 17:49:50 -0500

    not fixed, but will be

--- mc_creerCompte.php
+++ mc_creerCompte.php
@@ -10,10 +10,10 @@ if($resultat['nb'] == 0){
 $sql = "SELECT pk_specialite FROM specialite WHERE nom = '".$_POST['specialite']."'";
 $specialite = $db->query($sql)->fetch();
 $sql = "INSERT INTO utilisateur (utilisateur.nom, utilisateur.prenom, utilisateur.nb_session, utilisateur.loginId, utilisateur.image, utilisateur.email, utilisateur.specialite)
-        VALUES ('".$_POST['nom']."', '".$_POST['prenom']."', '".intval($_POST['nbSessions'])."', '".intval($_POST['id'])."', '".$_POST['image']."', '".$_POST['email']."', '".$specialite['pk_specialite']."');";
+        VALUES ('".$_POST['nom']."', '".$_POST['prenom']."', ".$_POST['nbSessions'].", ".$_POST['id'].", '".$_POST['image']."', '".$_POST['email']."', '".$specialite['pk_specialite']."');";
  $stmt = $db->prepare($sql);
  $stmt->execute();
  session_start();
- //unset($_SESSION['newUser']);
- //header("Location: ./feed.php?id={$_SESSION['id']}");
+ unset($_SESSION['newUser']);
+ header("Location: ./feed.php?id={$_SESSION['id']}");
 ?>
--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'bd.php';
-$sql = "SELECT * FROM utilisateur;";
-$resultat = $db->query($sql)->fetchAll();
-foreach ($resultat as $value) {
-  echo $value['email'];
-}
-?>
