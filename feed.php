a251706b4095367bacbb5dfe2f5829c2d4e065a4
TheMasterCado <master_cado@hotmail.com>
2018-03-06 12:04:13 -0500

    testing again

--- feed.php
+++ feed.php
@@ -18,6 +18,7 @@ require 'bd.php';
   </head>
   <body>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
   <script src="./JS/utils.js"></script>
 <?php if($_GET['id'] == $_SESSION['id']) { ?>
     <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#nouvellePublication">Nouvelle publication</button>
