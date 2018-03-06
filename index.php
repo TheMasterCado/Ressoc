91e1416f1dd9824e3948a47cdf6479e575453d9a
TheMasterCado <master_cado@hotmail.com>
2018-03-06 14:50:18 -0500

    styling

--- index.php
+++ index.php
@@ -12,32 +12,35 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script>
   <?php if(isset($_GET['signOut'])) { ?>
-  $(function() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut();
-  });
-  <?php } ?>
-  function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    $.post("./mc_connexion.php", {
-      id: profile.getId(),
-      prenom: profile.getGivenName(),
-      nom: profile.getFamilyName(),
-      email: profile.getEmail(),
-      image: profile.getImageUrl()
-    }, function(data) {
-      if (data == "NEW")
-        window.location.replace("./nouvelUtilisateur.php");
-      else {
-        window.location.replace("./feed.php?id=" + profile.getId());
-      }
+    $(function() {
+      gapi.load('auth2', function() {
+        gapi.auth2.init();
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut();
+      });
     });
-  }
-  </script>
-  <div id="content" class="center-block">
-    <h1>Ressoc</h1>
-    <h6>Un réseau social presque intéressant.</h6>
-    <div class="g-signin2" data-onsuccess="onSignIn"></div>
-  </div>
-</body>
-</html>
+    <?php } ?>
+    function onSignIn(googleUser) {
+      var profile = googleUser.getBasicProfile();
+      $.post("./mc_connexion.php", {
+        id: profile.getId(),
+        prenom: profile.getGivenName(),
+        nom: profile.getFamilyName(),
+        email: profile.getEmail(),
+        image: profile.getImageUrl()
+      }, function(data) {
+        if (data == "NEW")
+        window.location.replace("./nouvelUtilisateur.php");
+        else {
+          window.location.replace("./feed.php?id=" + profile.getId());
+        }
+      });
+    }
+    </script>
+    <div id="content" class="center-block">
+      <h1>Ressoc</h1>
+      <h6>Un réseau social presque intéressant.</h6>
+      <div class="g-signin2" data-onsuccess="onSignIn"></div>
+    </div>
+  </body>
+  </html>
