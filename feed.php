e276f89c7773177b516245b84d82f22157b164c8
TheMasterCado <master_cado@hotmail.com>
2018-03-06 11:36:24 -0500

    forgot bootstrap

--- feed.php
+++ feed.php
@@ -14,6 +14,7 @@ require 'bd.php';
   <head>
     <meta charset="utf-8">
     <title>feed</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   </head>
   <body>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
