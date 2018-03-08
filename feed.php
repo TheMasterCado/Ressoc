c8dcd1f561e8cfd5cace11601d7a2ea2ef379f83
TheMasterCado <master_cado@hotmail.com>
2018-03-08 11:47:53 -0500

    Footer in sidenav

--- feed.php
+++ feed.php
@@ -8,8 +8,13 @@ else {
   header("Location: ./index.php");
 }
 require 'bd.php';
-$sql = "SELECT prenom, nom FROM utilisateur WHERE loginID = '".$_GET['id']."';";
+$sql = "SELECT prenom, nom, pk_utilisateur FROM utilisateur WHERE loginID = '".$_GET['id']."';";
 $feedDe = $db->query($sql)->fetch();
+$sql = "SELECT prenom, nom, pk_utilisateur FROM utilisateur WHERE loginID = '".$_SESSION['id']."';";
+$currentUser = $db->query($sql)->fetch();
+$sql = "SELECT * FROM publication WHERE
+fk_utilisateur = ".$feedDe['pk_utilisateur']." AND fk_publication IS NULL;";
+$publications = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,40 +29,40 @@ $feedDe = $db->query($sql)->fetch();
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
   <script src="./JS/utils.js"></script>
   <script>
-    function traiterPoints(valeur, pk_publication) {
-      //todo
-    }
+  function traiterPoints(valeur, pk_publication) {
+    //todo
+  }
   </script>
   <div id="sidenav">
-    <h6>Feed de <?= $feedDe['prenom']." ".$feedDe['nom'] ?></h3>
+    <h6>Feed de <?= $feedDe['prenom']." ".$feedDe['nom'] ?></h6>
     <?php if($_GET['id'] == $_SESSION['id']) { ?>
       <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#nouvellePublication">Nouvelle publication</button>
     <?php } ?>
-
+    
     <a class="btn btn-info btn-sm" href="./feed.php?id=<?= $_SESSION['id'] ?>">Mon feed</a>
     <a class="btn btn-info btn-sm" href="./signOut.php">Se déconnecter</a>
-  </div>
-  <div id="main">
-    <?php
-    $sql = "SELECT * FROM publication WHERE fk_utilisateur =
-    (SELECT pk_utilisateur FROM utilisateur WHERE loginID = {$_GET['id']}) AND fk_publication IS NULL;";
-    $resultat = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-    foreach ($resultat as $pos => $publication) {
-    ?>
-      <div class='card'>
-        <div class="card-body">
-          <h6 class="card-subtitle mb-2 text-muted">
-            <?= 0 . " points - par " . $feedDe['prenom'] . " " . $feedDe['nom'] ?>
-          </h6>
-          <p class="card-text"><?= $publication['texte'] ?></p>
-          <a href="#" class="card-link text-success" onclick="traiterPoints(1, <?= $publication['pk_publication'] ?>)">Bien (+1)</a>
-          <a href="#" class="card-link text-danger" onclick="traiterPoints(-1, <?= $publication['pk_publication'] ?>)">Mauvais (-1)</a>
-          <a href="#" class="card-link">Commentaires</a>
-        </div>
+    <div class="sidenav-footer">
+      <h6>Connecté en tant que <br> <?= $currentUser['prenom']." ".$currentUser['nom'] ?></h6>
       </div>
-    <?php } ?>
-  </div>
+    </div>
+    <div id="main">
+      <?php
+      foreach ($publications as $pos => $publication) {
+        ?>
+        <div class='card'>
+          <div class="card-body">
+            <h6 class="card-subtitle mb-2 text-muted">
+              <?= 0 . " points - par " . $feedDe['prenom'] . " " . $feedDe['nom'] ?>
+            </h6>
+            <p class="card-text"><?= $publication['texte'] ?></p>
+            <a href="#" class="card-link text-success" onclick="traiterPoints(1, <?= $publication['pk_publication'] ?>)">Bien (+1)</a>
+            <a href="#" class="card-link text-danger" onclick="traiterPoints(-1, <?= $publication['pk_publication'] ?>)">Mauvais (-1)</a>
+            <a href="#" class="card-link">Commentaires</a>
+          </div>
+        </div>
+      <?php } ?>
+    </div>
 
-  <?php require "maPage.php"; ?>
-</body>
-</html>
+    <?php require "maPage.php"; ?>
+  </body>
+  </html>
