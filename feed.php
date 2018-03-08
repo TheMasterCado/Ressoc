7f57999469c0e47c9c90f47a9c95d8ab13fd08ed
TheMasterCado <master_cado@hotmail.com>
2018-03-08 15:01:02 -0500

    It wasn't working

--- feed.php
+++ feed.php
@@ -40,11 +40,11 @@ $publications = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }, function(data) {
         var points = parseInt($(el).parent().siblings(".card-subtitle").children("strong").text(), 10);
         if(valeur != 0)
-          points += valeur;
+          var diff += valeur;
         else
-          points += $(el).hasClass("vert") ? -1 : 1;
-        points *= ($(el).siblings(".selected").length == 0) ? 1 : 2;
-        $(el).parent().siblings(".card-subtitle").children("strong").text(points);
+          var diff += $(el).hasClass("vert") ? -1 : 1;
+        diff *= ($(el).siblings(".selected").length == 0) ? 1 : 2;
+        $(el).parent().siblings(".card-subtitle").children("strong").text(points + diff);
         deselectionner($(el).siblings(".card-link").eq(0));
         selectionner(el);
       });
