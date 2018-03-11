8e788a7ba8e16d8f28e6819e96f4c352c506cf9d
TheMasterCado <master_cado@hotmail.com>
2018-03-11 13:08:49 -0400

    Sql corections for quotes escaping

--- mc_creerCompte.php
+++ mc_creerCompte.php
@@ -4,19 +4,23 @@ if(!empty(trim($_POST['specialite']))) {
   $sql = "SELECT COUNT(*) AS nb FROM specialite WHERE nom = '".$_POST['specialite']."';";
   $resultat = $db->query($sql)->fetch();
   if($resultat['nb'] == 0){
-    $sql = "INSERT INTO specialite (specialite.nom) VALUES ('".$_POST['specialite']."');";
+    $sql = "INSERT INTO specialite (specialite.nom) VALUES (:specialite);";
     $stmt = $db->prepare($sql);
-    $stmt->execute();
+    $stmt->execute([':specialite' => $_POST['specialite']]);
   }
   $sql = "SELECT pk_specialite FROM specialite WHERE nom = '".$_POST['specialite']."'";
   $specialite = $db->query($sql)->fetch();
 }
 $sql = "INSERT INTO utilisateur (utilisateur.nom, utilisateur.prenom, utilisateur.nb_session,
         utilisateur.loginID, utilisateur.image, utilisateur.email".((isset($specialite)) ? ", publication.fk_specialite" : "").")
-        VALUES ('".$_POST['nom']."', '".$_POST['prenom']."', ".$_POST['nbSessions'].", '".$_POST['id']."',
+        VALUES (:nom, :prenom, ".$_POST['nbSessions'].", '".$_POST['id']."',
         '".$_POST['image']."', '".$_POST['email']."'".((isset($specialite))) ? ", ".$specialite['pk_specialite'] : "").");";
 $stmt = $db->prepare($sql);
-$stmt->execute();
+$params = [
+  ':prenom' => $_POST['prenom'],
+  ':nom' => $_POST['nom']
+];
+$stmt->execute($params);
 session_start();
 unset($_SESSION['newUser']);
 header("Location: ./feed.php?id={$_SESSION['id']}");
--- mc_creerPublication.php
+++ mc_creerPublication.php
@@ -5,9 +5,9 @@ if(!empty(trim($_POST['specialite']))) {
   $sql = "SELECT COUNT(*) AS nb FROM specialite WHERE nom = '".$_POST['specialite']."';";
   $resultat = $db->query($sql)->fetch();
   if($resultat['nb'] == 0){
-    $sql = "INSERT INTO specialite (specialite.nom) VALUES ('".$_POST['specialite']."');";
+    $sql = "INSERT INTO specialite (specialite.nom) VALUES (:specialite);";
     $stmt = $db->prepare($sql);
-    $stmt->execute();
+    $stmt->execute([':specialite' => $_POST['specialite']]);
   }
   $sql = "SELECT pk_specialite FROM specialite WHERE nom = '".$_POST['specialite']."'";
   $specialite = $db->query($sql)->fetch();
@@ -19,9 +19,13 @@ $fk_utilisateur = $db->query($sql)->fetch();
 $sql = "INSERT INTO publication (publication.texte, publication.fk_type_publication,
         publication.fk_utilisateur".((isset($specialite)) ? ", publication.fk_specialite" : "").
         ((isset($_POST['parent'])) ? ", publication.fk_publication" : "").")
-        VALUES ('".$_POST['contenu']."', ".$fk_type_publication['pk_type_publication'].", ".
-        $fk_utilisateur['pk_utilisateur'].((isset($specialite)) ? ", ".$specialite['pk_specialite'] : "").
+        VALUES (:contenu, :type_pub, :fk_utilisateur".((isset($specialite)) ? ", ".$specialite['pk_specialite'] : "").
         ((isset($_POST['parent'])) ? ", ".$_POST['parent'] : "").");";
 $stmt = $db->prepare($sql);
-$stmt->execute();
+$params = [
+  ':contenu' => $_POST['contenu'],
+  ':type_pub' => $fk_type_publication['pk_type_publication'],
+  ':fk_utilisateur' => $fk_utilisateur['pk_utilisateur']
+];
+$stmt->execute($params);
 ?>
