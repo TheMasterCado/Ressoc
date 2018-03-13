4b925050c5474a9b4fb85b2d9fdf1b0e068c7b9c
TheMasterCado <master_cado@hotmail.com>
2018-03-13 16:00:39 -0400

    okay maybe

--- php_utils.php
+++ php_utils.php
@@ -8,11 +8,13 @@ function markUp($pattern, $replaceBy, $text, $omit = NULL) {
         if($pos % 2 == 0) {
           $sections = explode($pattern, $section);
           foreach ($sections as $pospos => $valeur) {
-            if($pospos % 2 == 1)
+            if($pospos % 2 == 1) {
               $newText .= $replaceBy[0];
+            }
             $newText .= $valeur;
-            if($pospos % 2 == 1)
+            if($pospos % 2 == 1) {
               $newText .= $replaceBy[1];
+            }
           }
         }
         else {
@@ -34,6 +36,47 @@ function markUp($pattern, $replaceBy, $text, $omit = NULL) {
   return $newText;
 }
 
+function markUpLinks($patternStart, $patternEnd, $text, $omit = NULL) {
+  $newText = "";
+  if(!empty($omit)) {
+    foreach ($omit as $i => $o) {
+      $biggerSections = explode($o, $text);
+      foreach ($biggerSections as $pos => $section) {
+        if($pos % 2 == 0) {
+          $occ = explode($patternStart, $section);
+          array_splice($occ, 0, 1);
+          foreach ($occ as $pospos => $valeur) {
+            $link = explode($patternEnd, $occ);
+            if(count($link) > 1) {
+              $newText .= "<a href=\"" . $link[0] . "\">" .
+                           $link[0] . "</a>";
+              array_splice($link, 0, 1);
+              $newText .= implode($link);
+            }
+            else
+              $newText .= $patternStart . $occ;
+          }
+        }
+        else {
+          $newText .= $o . $section . $o;
+        }
+      }
+    }
+  }
+  else {
+    $sections = explode($pattern, $text);
+    foreach ($sections as $pospos => $valeur) {
+      if($pospos % 2 == 1)
+        $newText .= $replaceBy[0];
+      $newText .= $valeur;
+      if($pospos % 2 == 1)
+        $newText .= $replaceBy[1];
+    }
+  }
+  return $newText;
+}
+}
+
 function formatEverything($string) {
   $text = markUp("**", ['<strong>', '</strong>'], $string, ["@@"]);
   $text = markUp("//", ['<em>', '</em>'], $text, ["@@"]);
@@ -41,6 +84,7 @@ function formatEverything($string) {
   $text = markUp("__", ['<ins>', '</ins>'], $text, ["@@"]);
   $text = markUp("^^", ['<sup>', '</sup>'], $text, ["@@"]);
   $text = markUp("##", ['<mark>', '</mark>'], $text, ["@@"]);
+  $text = markUpLinks("[", "]", $text, ["@@"]);
   $text = markUp("@@", ['<code>', '</code>'], $text);
   return $text;
 }
