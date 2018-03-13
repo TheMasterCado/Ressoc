f56dc1a1536aa4f132cc37ec4b59e409ae0134ee
TheMasterCado <master_cado@hotmail.com>
2018-03-13 11:09:04 -0400

    commit

--- sidenav.php
+++ sidenav.php
@@ -3,17 +3,23 @@
 $sql = "SELECT prenom, nom, loginID FROM utilisateur ORDER BY prenom ASC;";
 $allUsers = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 //Infos de l'utilisateur connecté
-$sql = "SELECT prenom, nom, pk_utilisateur FROM utilisateur WHERE loginID = '".$_SESSION['id']."';";
-$currentUser = $db->query($sql)->fetch();
+$sql = "SELECT prenom, nom, pk_utilisateur FROM utilisateur WHERE loginID = :id;";
+$stmt = $db->prepare($sql);
+$stmt->execute([':id'] => $_SESSION['id']);
+$currentUser = $stmt->fetch();
 //Spécialité de l'utilisateur à qui le feed appartient
-$sql = "SELECT nom FROM specialite WHERE pk_specialite = '".$feedDe['specialite']."';";
-$specialiteUser = $db->query($sql)->fetch();
+if($feedDe['specialite'] != NULL) {
+$sql = "SELECT nom FROM specialite WHERE pk_specialite = :specialite;";
+$stmt->prepare($sql);
+$stmt->execute([':specialite' => $feedDe['fk_specialite']]);
+$specialiteUser = $stmt->fetch();
+}
  ?>
  <div id="sidenav">
    <h6><?= $titre ?><br><?= $feedDe['prenom']." ".$feedDe['nom'] ?></h6>
    <img src="<?= $feedDe['image'] ?>">
    <p>Spécialité: <strong><?= $specialiteUser ?></strong></p>
-   <p>Nombre de sessions: <strong><?= $feedDe['nbSessions'] ?></strong></p>
+   <p>Nombre de sessions: <strong><?= $feedDe['nb_session'] ?></strong></p>
    <div id="sidenav-buttons">
      <?php if($_GET['id'] == $_SESSION['id']) { ?>
        <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#nouvellePublication">Nouvelle publication</button>
