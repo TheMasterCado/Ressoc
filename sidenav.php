2687dbdc26a769ce85b52601365158483be9cde9
TheMasterCado <master_cado@hotmail.com>
2018-03-13 11:12:51 -0400

    bug

--- sidenav.php
+++ sidenav.php
@@ -18,7 +18,7 @@ $specialiteUser = $stmt->fetch();
  <div id="sidenav">
    <h6><?= $titre ?><br><?= $feedDe['prenom']." ".$feedDe['nom'] ?></h6>
    <img src="<?= $feedDe['image'] ?>">
-   <p>Spécialité: <strong><?= ((isset($specialiteUser['nom']) ? $specialiteUser['nom'] : "Aucune") ?></strong></p>
+   <p>Spécialité: <strong><?= (isset($specialiteUser['nom']) ? $specialiteUser['nom'] : "Aucune") ?></strong></p>
    <p>Nombre de sessions: <strong><?= $feedDe['nb_session'] ?></strong></p>
    <div id="sidenav-buttons">
      <?php if($_GET['id'] == $_SESSION['id']) { ?>
