e2205d8a7ad07be332327bc17127dfd5d6217b9b
TheMasterCado <master_cado@hotmail.com>
2018-03-13 11:18:03 -0400

    sql

--- mc_traiterVotes.php
+++ mc_traiterVotes.php
@@ -12,10 +12,10 @@ $stmt->execute([':pub' => $_POST['pub'], ':pk_utilisateur' => $utilisateur['pk_u
 $resultat = $stmt->fetch();
 if($resultat['nb'] == 0)
   $sql = "INSERT INTO vote (vote.fk_publication, vote.fk_utilisateur, vote.valeur)
-          VALUES (:pub, :pk_utilisateur, :val;";
+          VALUES (:pub, :pk_utilisateur, :val);";
 else
   $sql = "UPDATE vote SET valeur = :val WHERE
-          fk_publication = :pub AND fk_utilisateur = :pk_specialite;";
+          fk_publication = :pub AND fk_utilisateur = :pk_utilisateur;";
 $stmt = $db->prepare($sql);
 $params = [
   ':val' => $_POST['val'],
