bcb7addd15ce34f40654d295e1a8c1d0f40d1320
TheMasterCado <master_cado@hotmail.com>
2018-03-15 00:03:17 -0400

    Okay

--- feed.php
+++ feed.php
@@ -128,11 +128,12 @@ $titre2 = $feedDe['prenom']." ".$feedDe['nom'];
   <div id="main">
     <div id="filter">
       <div class="stay-right">
-        <label for="ordre">Classer par <select name="ordre" class="form-control discreet-dropdown">
+        <label for="ordre">Classer par </label>
+        <select name="ordre" class="form-control discreet-dropdown">
           <option value="date">Date</option>
           <option value="points">Points</option>
           <option value="m" selected>m</option>
-        </select></label>
+        </select>
       </div>
     </div>
     <?php
