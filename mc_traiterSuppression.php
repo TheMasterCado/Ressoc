a866e67578609d1d773e5d4636086ff14cffa3bf
TheMasterCado <master_cado@hotmail.com>
2018-03-20 14:51:21 -0400

    secority

--- mc_traiterSuppression.php
+++ mc_traiterSuppression.php
@@ -1,13 +1,23 @@
 <?php
 session_start();
 require 'bd.php';
-if(!isset($_POST['pk_publication']) || !isset($_SESSION['id']) || !($_SESSION['id'] == $_POST['id'])) {
+if(!isset($_POST['pk_publication']) || !isset($_SESSION['id']))
   header("Location: ./index.php");
-}
-  $sql = "DELETE FROM publication WHERE pk_publication = :pub;";
-  $stmt = $db->prepare($sql);
-  $params = [
-    ':pub' => $_POST['pk_publication']
-  ];
-  $stmt->execute($params);
+$sql = "SELECT loginID FROM utilisateur
+        INNER JOIN publication ON fk_utilisateur = pk_utilisateur
+        WHERE pk_publication = :pub;";
+$stmt = $db->prepare($sql);
+$params = [
+  ':pub' => $_POST['pk_publication']
+];
+$stmt->execute($params);
+$author = $stmt->fetch();
+if($stmt['loginID'] != $_SESSION['id'])
+  header("Location: ./index.php");  
+$sql = "DELETE FROM publication WHERE pk_publication = :pub;";
+$stmt = $db->prepare($sql);
+$params = [
+  ':pub' => $_POST['pk_publication']
+];
+$stmt->execute($params);
  ?>
