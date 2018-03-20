94bfe9419e8da92a131225c760a0956fd7824d96
Dubois Jonathan <1635764@cegepthetford.ca>
2018-03-19 21:27:52 -0400

    quick fix

--- publication.php
+++ publication.php
@@ -206,7 +206,6 @@ $titre2 = $feedDe['prenom']." ".$feedDe['nom'];
         </div>
         <div id="commentaires">
       <?php
-      $bonneReponse = null;
       foreach ($commentaires as $pos => $commentaire){
         if($commentaire['description'] = 'BonneReponse')
           $bonneReponse = $commentaire;
@@ -219,7 +218,7 @@ $titre2 = $feedDe['prenom']." ".$feedDe['nom'];
             <strong><?= $commentaire['points'] ?></strong> points - par <?= $commentaire['prenom'] . " " . $commentaire['nom'] . " - " ?>
             <span class="timestamp"><?= time_ago($commentaire['timestamp']) ?></span>
             <?php
-            if($currentUser['pk_utilisateur'] == $publication['fk_utilisateur'] && ($bonneReponse == null || $commentaire == $bonneReponse)){
+            if($currentUser['pk_utilisateur'] == $publication['fk_utilisateur'] && (isset($bonneReponse) || $commentaire == $bonneReponse)){
               ?>
             <a href="javascript:void(null);" onclick="traiterBonneReponse(<?= $commentaire['pk_publication'] ?>, '<?= $commentaire['description'] ?>')">
               <img src=<?= ($commentaire['description'] == 'BonneReponse') ? "./Images/glyphicons/png/glyphicons-208-remove.png" : "./Images/glyphicons/png/glyphicons-153-check.png" ?> class="glyph">
