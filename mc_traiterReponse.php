0083e66f2ff409c43b7b1da4f6b2582526e0559a
Dubois Jonathan <1635764@cegepthetford.ca>
2018-03-19 22:03:11 -0400

    update parent

--- mc_traiterReponse.php
+++ mc_traiterReponse.php
@@ -11,4 +11,11 @@ $params = [
 ];
 $stmt->execute($params);
 
+$sql = "UPDATE publication SET fk_type_publication = (SELECT pk_type_publication from type_publication WHERE description = :description) WHERE pk_publication = :pub;";
+$stmt = $db->prepare($sql);
+$params = [
+  ':pub' => $_POST['pk_publication'],
+  ':description' => ($_POST['type_publication'] == 'Texte') ? 'QuestionRepondue' : 'Texte'
+];
+$stmt->execute($params);
  ?>
