49214d149b87a1a517f38f03d6c0e707edb32348
TheMasterCado <master_cado@hotmail.com>
2018-03-20 13:13:29 -0400

    test

--- mc_traiterSuppression.php
+++ mc_traiterSuppression.php
@@ -1,8 +1,10 @@
 <?php
 require 'bd.php';
-if(!isset($_POST['pk_publication']) || $_SESSION['id'] != $_POST['id']){
-  header("Location: ./index.php");
-}
+// if(!isset($_POST['pk_publication']) || $_SESSION['id'] != $_POST['id']){
+//   header("Location: ./index.php");
+// }
+echo $_SESSION['id'];
+echo $_POST['id'];
 $sql = "DELETE FROM publication WHERE pk_publication = :pub;";
 $stmt = $db->prepare($sql);
 $params = [
