c5092550777ebae81d3e1d2a076807d76ba73e92
TheMasterCado <master_cado@hotmail.com>
2018-03-21 15:36:35 -0400

    asd

--- nouvelUtilisateur.php
+++ nouvelUtilisateur.php
@@ -16,32 +16,33 @@ header("Location: ./index.php");
   <link rel="stylesheet" type="text/css" href="./CSS/nouvelUtilisateur.css">
 </head>
 <body>
-<h1>Créer un compte</h1>
-<form id="formulaire" class="" action="./mc_creerCompte.php" method="post">
-  <table>
-    <tr>
-  <td><label for="prenom">Prénom</label></td>
-  <td><input type="text" name="prenom" value="<?=$_SESSION['newUser']['prenom']?>" required></td>
-</tr>
-    <tr>
-  <td><label for="nom">Nom</label></td>
-  <td><input type="text" name="nom" value="<?=isset($_SESSION['newUser']['nom']) ? $_SESSION['newUser']['nom'] : ""?>" required></td>
-</tr>
-    <tr>
-<td><label for="nbSessions">Nombre de sessions en informatique</label></td>
-<td><input type="number" name="nbSessions" min="1" max="6" required></td>
-</tr>
-    <tr>
-<td><label for="specialite">Spécialité (facultatif)</label></td>
-  <td><input type="text" name="specialite"></td>
-</tr>
-  <input type="hidden" name="email" value="<?=$_SESSION['newUser']['email']?>">
-  <input type="hidden" name="image" value="<?=$_SESSION['newUser']['image']?>">
-  <input type="hidden" name="id" value="<?=$_SESSION['id']?>">
+  <h1>Créer un compte</h1>
+  <form id="formulaire" class="" action="./mc_creerCompte.php" method="post">
+    <table>
       <tr>
-  <td><input type="submit" value="Valider"></td>
-</tr>
+        <td><label for="prenom">Prénom :</label></td>
+        <td><input class="form-control" type="text" name="prenom" value="<?=$_SESSION['newUser']['prenom']?>" required></td>
+      </tr>
+      <tr>
+        <td><label for="nom">Nom :</label></td>
+        <td><input class="form-control" type="text" name="nom" value="<?=isset($_SESSION['newUser']['nom']) ? $_SESSION['newUser']['nom'] : ""?>" required></td>
+      </tr>
+      <tr>
+        <td><label for="nbSessions">Nombre de sessions en informatique :</label></td>
+        <td><input class="form-control" type="number" name="nbSessions" min="1" max="6" required></td>
+      </tr>
+      <tr>
+        <td><label for="specialite">Spécialité (facultatif) :</label></td>
+        <td><input class="form-control" type="text" name="specialite"></td>
+      </tr>
+      <input type="hidden" name="email" value="<?=$_SESSION['newUser']['email']?>">
+      <input type="hidden" name="image" value="<?=$_SESSION['newUser']['image']?>">
+      <input type="hidden" name="id" value="<?=$_SESSION['id']?>">
+      <tr>
+        <td></td>
+        <td><input type="submit" value="Valider"></td>
+      </tr>
     </table>
-</form>
+  </form>
 </body>
 </html>
