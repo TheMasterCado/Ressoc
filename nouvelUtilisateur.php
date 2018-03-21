a9ece3fcce61a033c1729446e4d75777db9a3d9b
TheMasterCado <master_cado@hotmail.com>
2018-03-21 15:51:31 -0400

    s

--- nouvelUtilisateur.php
+++ nouvelUtilisateur.php
@@ -21,19 +21,19 @@ header("Location: ./index.php");
     <table>
       <tr>
         <td><label for="prenom">Prénom :</label></td>
-        <td><input class="form-control" type="text" name="prenom" value="<?=$_SESSION['newUser']['prenom']?>" required></td>
+        <td><input type="text" name="prenom" value="<?=$_SESSION['newUser']['prenom']?>" required></td>
       </tr>
       <tr>
         <td><label for="nom">Nom :</label></td>
-        <td><input class="form-control" type="text" name="nom" value="<?=isset($_SESSION['newUser']['nom']) ? $_SESSION['newUser']['nom'] : ""?>" required></td>
+        <td><input type="text" name="nom" value="<?=isset($_SESSION['newUser']['nom']) ? $_SESSION['newUser']['nom'] : ""?>" required></td>
       </tr>
       <tr>
         <td><label for="nbSessions">Nombre de sessions en informatique :</label></td>
-        <td><input class="form-control" type="number" name="nbSessions" min="1" max="6" required></td>
+        <td><input type="number" name="nbSessions" min="1" max="6" required></td>
       </tr>
       <tr>
         <td><label for="specialite">Spécialité (facultatif) :</label></td>
-        <td><input class="form-control" type="text" name="specialite"></td>
+        <td><input type="text" name="specialite"></td>
       </tr>
       <input type="hidden" name="email" value="<?=$_SESSION['newUser']['email']?>">
       <input type="hidden" name="image" value="<?=$_SESSION['newUser']['image']?>">
