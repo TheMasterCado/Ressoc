452228317f1fd33d130000df88a3e6212c135894
TheMasterCado <master_cado@hotmail.com>
2018-03-27 15:14:17 -0400

    ss

--- mc_connexion.php
+++ mc_connexion.php
@@ -2,7 +2,7 @@
   require 'bd.php';
   require_once 'google-api-php-client-2.2.1/vendor/autoload.php';
   session_start();
-  $client = new Google_Client(['client_id' => 374009514589-ie80vs5damvpplc85uni3ep2emi64f0b.apps.googleusercontent.com]);
+  $client = new Google_Client(['client_id' => '374009514589-ie80vs5damvpplc85uni3ep2emi64f0b.apps.googleusercontent.com']);
   $payload = $client->verifyIdToken($_POST['token']);
   if ($payload) {
     $id = hash("sha256", $payload['sub']);
